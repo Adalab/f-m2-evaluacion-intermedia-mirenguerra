b0b2fcb39b30b578a5b6e4e3384a43d6d713b949
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-12 10:13:12 +0200

    Fix errors

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -29,7 +29,7 @@ console.log('Número aleatorio: ' + aleatoryNumber);
 function getRandomNumber() {
     return Math.ceil(Math.random() * 100);  
   } 
-
+// Crear handler para el botón de volver a jugar. ARREGLAR!! PONGO EL CONTADOR A CERO PERO SUMA LOS CLICKS DE LA PARTIDA ANTERIOR!!!
 function handleRestartButtonClick(){
     testNumberEl.value = '';
     testCounterEl.innerHTML = 0;
@@ -39,28 +39,25 @@ function handleRestartButtonClick(){
     testButton.addEventListener('click', handleButtonClick);
 }
 
-
-// Escuchar click en botón
+// Escuchar click en botón Prueba
 testButton.addEventListener('click', handleButtonClick);
 
-// Crear handler de click en botón
+// Crear handler de click en botón Prueba
 function handleButtonClick() {
     // Recoger valor de input en este momento y convertirlo a número
     let userNumber = parseInt(testNumberEl.value);
 
     // Si número aleatorio es correcto
-     // Escribir  "¡HAS GANADO, CAMPEONA!" en div de feedback y que salga un botón para volver a jugar y se ejecute la función de ese botón cuando se haga click
+     // Escribir  "¡HAS GANADO, CAMPEONA!" en div de feedback y que salga un botón para volver a jugar y se ejecute la función de ese botón cuando se haga click. Escuchar el click del botón de volver a jugar
     if (userNumber===aleatoryNumber){
         testFeedbackEl.innerHTML = '¡HAS GANADO!' + '<button class="restart__btn">¡Juega otra vez!</button>';
         const winButtonEl = document.querySelector('.restart__btn');
-        winButtonEl.addEventListener('click', handleRestartButtonClick);
-        
+        winButtonEl.addEventListener('click', handleRestartButtonClick);   
     }   
     // Si es mayor de 100 o menor de 1 que diga que ese número no es válido
     else if(userNumber<1 || userNumber>100){
         testFeedbackEl.innerHTML = 'Ese número no vale. Prueba con uno del 1 al 100.';
     }
-   
     // Si mayor que número aleatorio
         // Escribir  "demasiado alto" en div de feedback
     else if (userNumber>aleatoryNumber){
@@ -84,5 +81,7 @@ function handleButtonClick() {
     if (parseInt(counter)>=100){
         testCounterEl.innerHTML = '+99';
     }
-    
 }
+
+
+
