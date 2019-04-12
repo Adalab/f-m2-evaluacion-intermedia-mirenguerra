c1cdb474f3b3a3024116a3003ced9c59975d3739
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-12 10:00:06 +0200

    Add functions to restart button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Se trata de un juego que consiste en adivinar un número aleatorio (oculto) del 1 al 100, nos van dando pistas de si es muy bajo o muy alto y hay un contador de intentos que llevamos.
 // En la pantalla hay un input para intriducir el número, un botón para probar si he acertado, un espacio para el feedback y un espacio para el contador de intentos.
 // Usamos las funciones Math.random y Math.ceil par crear el número aleatorio.
@@ -14,33 +15,38 @@ const testCounterEl = document.querySelector('.test__counter');
 // Recoger botón de prueba
 const testButton = document.querySelector('.test__btn');
 // Recoger resultado de la función getRandomNumber
-const aleatoryNumber = getRandomNumber();
-// Recoger botón de volver a jugar
-// const winButtonEl = document.querySelector('.restart__btn');
+let aleatoryNumber = getRandomNumber();
+
 // Generar contador 
 let counter = 0;
 
 // Comprobar que hasta aquí va bien
 // console.log(testNumberEl, testFeedbackEl, testCounterEl, testButton, winButtonEl);
 // Me interesa ver el número para hacer pruebas ahora
-console.log(aleatoryNumber);
+console.log('Número aleatorio: ' + aleatoryNumber);
 
 // Generar número aleatorio (Math.random y Math.ceil)
 function getRandomNumber() {
     return Math.ceil(Math.random() * 100);  
   } 
 
-  
+function handleRestartButtonClick(){
+    testNumberEl.value = '';
+    testCounterEl.innerHTML = 0;
+    testFeedbackEl.innerHTML ='Escribe un número y dale a Prueba';
+    aleatoryNumber = getRandomNumber();
+    console.log('Nuevo número aleatorio: ' + aleatoryNumber);
+    testButton.addEventListener('click', handleButtonClick);
+}
+
+
 // Escuchar click en botón
 testButton.addEventListener('click', handleButtonClick);
 
 // Crear handler de click en botón
 function handleButtonClick() {
-// En este punto compruebo
-// console.log('CLICK');
-
     // Recoger valor de input en este momento y convertirlo a número
-    const userNumber = parseInt(testNumberEl.value);
+    let userNumber = parseInt(testNumberEl.value);
 
     // Si número aleatorio es correcto
      // Escribir  "¡HAS GANADO, CAMPEONA!" en div de feedback y que salga un botón para volver a jugar y se ejecute la función de ese botón cuando se haga click
@@ -48,10 +54,7 @@ function handleButtonClick() {
         testFeedbackEl.innerHTML = '¡HAS GANADO!' + '<button class="restart__btn">¡Juega otra vez!</button>';
         const winButtonEl = document.querySelector('.restart__btn');
         winButtonEl.addEventListener('click', handleRestartButtonClick);
-        function handleRestartButtonClick(){
-            counter = 0;
-            handleButtonClick();
-        }
+        
     }   
     // Si es mayor de 100 o menor de 1 que diga que ese número no es válido
     else if(userNumber<1 || userNumber>100){
@@ -68,8 +71,8 @@ function handleButtonClick() {
     else if(userNumber<aleatoryNumber){
         testFeedbackEl.innerHTML = 'Demasiado bajo.';
     }
-    // Si no se cumple nada de lo anterior que diga que ese número no es válido
-    else{
+    // Si no se escrube nada que diga que no se ha puesto ningún número
+    else if(isNaN(userNumber)){
         testFeedbackEl.innerHTML = 'No has puesto ningún número. Prueba con uno del 1 al 100.';
     }
    
@@ -81,7 +84,5 @@ function handleButtonClick() {
     if (parseInt(counter)>=100){
         testCounterEl.innerHTML = '+99';
     }
+    
 }
-
-
-
