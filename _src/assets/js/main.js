1ceb284c2bab7c9d75938385812ace7c14a95715
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-12 13:06:28 +0200

    Add prettier and remove favicon

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -17,7 +17,7 @@ const testButton = document.querySelector('.test__btn');
 // Recoger resultado de la función getRandomNumber
 let aleatoryNumber = getRandomNumber();
 
-// Generar contador 
+// Generar contador
 let counter = 0;
 
 // Comprobar que hasta aquí va bien
@@ -27,16 +27,16 @@ console.log('Número aleatorio: ' + aleatoryNumber);
 
 // Generar número aleatorio (Math.random y Math.ceil)
 function getRandomNumber() {
-    return Math.ceil(Math.random() * 100);  
-  } 
+  return Math.ceil(Math.random() * 100);
+}
 // Crear handler para el botón de volver a jugar. ARREGLAR!! PONGO EL CONTADOR A CERO PERO SUMA LOS CLICKS DE LA PARTIDA ANTERIOR!!!
 function handleRestartButtonClick(){
-    testNumberEl.value = '';
-    testCounterEl.innerHTML = 0;
-    testFeedbackEl.innerHTML ='Escribe un número y dale a Prueba';
-    aleatoryNumber = getRandomNumber();
-    console.log('Nuevo número aleatorio: ' + aleatoryNumber);
-    testButton.addEventListener('click', handleButtonClick);
+  testNumberEl.value = '';
+  testCounterEl.innerHTML = 0;
+  testFeedbackEl.innerHTML ='Escribe un número y dale a Prueba';
+  aleatoryNumber = getRandomNumber();
+  console.log('Nuevo número aleatorio: ' + aleatoryNumber);
+  testButton.addEventListener('click', handleButtonClick);
 }
 
 // Escuchar click en botón Prueba
@@ -44,43 +44,42 @@ testButton.addEventListener('click', handleButtonClick);
 
 // Crear handler de click en botón Prueba
 function handleButtonClick() {
-    // Recoger valor de input en este momento y convertirlo a número
-    let userNumber = parseInt(testNumberEl.value);
+  // Recoger valor de input en este momento y convertirlo a número
+  let userNumber = parseInt(testNumberEl.value);
 
-    // Si número aleatorio es correcto
-     // Escribir  "¡HAS GANADO, CAMPEONA!" en div de feedback y que salga un botón para volver a jugar y se ejecute la función de ese botón cuando se haga click. Escuchar el click del botón de volver a jugar
-    if (userNumber===aleatoryNumber){
-        testFeedbackEl.innerHTML = '¡HAS GANADO!' + '<button class="restart__btn">¡Juega otra vez!</button>';
-        const winButtonEl = document.querySelector('.restart__btn');
-        winButtonEl.addEventListener('click', handleRestartButtonClick);   
-    }   
-    // Si es mayor de 100 o menor de 1 que diga que ese número no es válido
-    else if(userNumber<1 || userNumber>100){
-        testFeedbackEl.innerHTML = 'Ese número no vale. Prueba con uno del 1 al 100.';
-    }
-    // Si mayor que número aleatorio
-        // Escribir  "demasiado alto" en div de feedback
-    else if (userNumber>aleatoryNumber){
-        testFeedbackEl.innerHTML = 'Demasiado alto.';
-    }
-    // Si no
-        //Escribir "demasiado bajo" en div de feedback
-    else if(userNumber<aleatoryNumber){
-        testFeedbackEl.innerHTML = 'Demasiado bajo.';
-    }
-    // Si no se escrube nada que diga que no se ha puesto ningún número
-    else if(isNaN(userNumber)){
-        testFeedbackEl.innerHTML = 'No has puesto ningún número. Prueba con uno del 1 al 100.';
-    }
-   
-    console.log(userNumber);
-    // Aumentar el contador que sume uno cada vez que se haga click
-    counter += 1;
-    testCounterEl.innerHTML = counter;
-    // Limitar el contador para que no lleguen a ser tres cifras y a partir de ahí ponga '+99' y así no se salga del spinner
-    if (parseInt(counter)>=100){
-        testCounterEl.innerHTML = '+99';
-    }
+  // Si número aleatorio es correcto
+  // Escribir  "¡HAS GANADO, CAMPEONA!" en div de feedback y que salga un botón para volver a jugar y se ejecute la función de ese botón cuando se haga click. Escuchar el click del botón de volver a jugar
+  if (userNumber===aleatoryNumber){
+    testFeedbackEl.innerHTML = '¡HAS GANADO!' + '<button class="restart__btn">¡Juega otra vez!</button>';
+    const winButtonEl = document.querySelector('.restart__btn');
+    winButtonEl.addEventListener('click', handleRestartButtonClick);
+  }
+  // Si es mayor de 100 o menor de 1 que diga que ese número no es válido
+  else if(userNumber<1 || userNumber>100){
+    testFeedbackEl.innerHTML = 'Ese número no vale. Prueba con uno del 1 al 100.';
+  }
+  // Si mayor que número aleatorio
+  // Escribir  "demasiado alto" en div de feedback
+  else if (userNumber>aleatoryNumber){
+    testFeedbackEl.innerHTML = 'Demasiado alto.';
+  }
+  // Si no
+  //Escribir "demasiado bajo" en div de feedback
+  else if(userNumber<aleatoryNumber){
+    testFeedbackEl.innerHTML = 'Demasiado bajo.';
+  }
+  // Si no se escrube nada que diga que no se ha puesto ningún número
+  else if(isNaN(userNumber)){
+    testFeedbackEl.innerHTML = 'No has puesto ningún número. Prueba con uno del 1 al 100.';
+  }
+
+  // Aumentar el contador que sume uno cada vez que se haga click
+  counter += 1;
+  testCounterEl.innerHTML = counter;
+  // Limitar el contador para que no lleguen a ser tres cifras y a partir de ahí ponga '+99' y así no se salga del spinner
+  if (parseInt(counter)>=100){
+    testCounterEl.innerHTML = '+99';
+  }
 }
 
 
